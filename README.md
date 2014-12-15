54de4b1043af476cc67efe5ccd1ffe7ee8f73332
loadingvx <loadingvx@163.com>
2014-12-15 14:45:24 +0800

    1.  Tabular: Ascii table render

---
+++ README.md
@@ -0,0 +1,36 @@
+PandoraBox
+==========
+
+A toolbox for daily debuging and programming.
+
+##Tabular
+A ascii table render.
+* Use function 'row' to add one row to table
+* Call 'render' to show final table
+
+Usage:
+
+```python
+	import tabular
+	t = tabular.Tabular()
+	t.row({'id':3, 'name':'Michael Jackson',  'age':'28'})
+	t.row({'id':2, 'name':'Pandora Avatar', 'age':'28'})
+	t.row({'id':1, 'name':'Jack Jones', 'age':'28'})
+	t.row({'id':1, 'name':'路人甲', 'age':'28'})
+	t.render('id')
+```
+
+output:
+<pre>
+	+---+----+-----------------+
+	| id| age| name            |
+	+---+----+-----------------+
+	| 3 | 28 | Michael Jackson |
+	| 2 | 28 | Pandora Avatar  |
+	| 1 | 28 | Jack Jones      |
+	| 1 | 28 | 路人甲           |
+	+---+----+-----------------+
+<pre>
+
+
+
---
+++ tabular.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+#encoding:utf-8
+
+
+import sys
+
+
+def is_chinese(uchar):
+    if uchar >= u'\u4e00' and uchar <= u'\u9fa5':
+        return True
+    return False
+
+
+class Tabular(object):
+
+    def __init__(self):
+        self.rows      = []
+        self.col_width = {}
+
+
+    def row(self, row):
+        str_row = {}
+        for k in row:
+            str_row[str(k).decode("UTF-8")] = str(row[k]).decode('UTF-8')
+        self.rows.append(str_row)
+
+        for h in str_row:
+            current_wid = self.view_width(str_row[h])
+            if h not in self.col_width:
+                self.col_width[h] = self.view_width(h)
+            if self.col_width[h] < current_wid:
+                self.col_width[h] = current_wid
+
+    def view_width(self, label):
+        width = 0
+        for e in label:
+            if is_chinese(e):
+                width += 2
+            else:
+                width += 1
+        return width
+
+    def unicode_cnt(self, ustring):
+        cnt = 0
+        for each in ustring:
+            if is_chinese(each):
+                cnt+= 1
+        return cnt
+
+
+
+    def render(self, primary_key='', max_lines=-1):
+        bar = ''
+        content = ''
+        if not primary_key:
+            primary_key = self.col_width.keys()[0]
+        if primary_key not in self.col_width:
+            print>>sys.stderr, "[WARN] Unknow colume name '%s'"%primary_key
+        else:
+            content = '| %s'%(primary_key.ljust(self.col_width[primary_key]-self.unicode_cnt(primary_key)))
+            bar+= '+-%s'%('-'*self.col_width[primary_key])
+        keys = self.col_width.keys()
+        keys.sort()
+        for each in keys:
+            if each == primary_key:
+                continue
+            bar+= '+-%s'%('-'*self.col_width[each])
+            content += '| %s'%(each.ljust(self.col_width[each]-self.unicode_cnt(each)))
+        bar+= "-+"
+        content += " |"
+        print bar
+        print content.encode('UTF-8')
+        print bar
+
+        line_cnt = 0
+        for line in self.rows:
+            if primary_key in self.col_width:
+                if primary_key in line:
+                    content = '| %s'%(line[primary_key].ljust(self.col_width[primary_key]- self.unicode_cnt(line[primary_key])))
+                else:
+                    content = '| %s'%(''.ljust(self.col_width[primary_key]))
+            for k in keys:
+                if k == primary_key:
+                    continue
+                if k in line:
+                    content += '| %s'%(line[k].ljust(self.col_width[k]-self.unicode_cnt(line[k])))
+                else:
+                    content += '| %s'%(''.ljust(self.col_width[k]))
+            content += ' |'
+            if line_cnt > max_lines and max_lines > 0:
+                print bar
+                print 'top of %d lines'%(max_lines)
+                return
+            line_cnt+= 1
+            print content.encode('UTF-8')
+        print bar
+
+
+if __name__ == "__main__":
+    t = Tabular()
+    t.row({        'name':'John',  'age':'28'})
+    t.row({'id':2,                 'age':'29'})
+    t.row({'id':1, 'name':'Peter', 'age':'28'})
+    t.render('id')
+
+
+
+
